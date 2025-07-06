cb1f91286335d70405f1c4184881290098a23f94
elishamutang <elishamutang@outlook.com>
2025-07-06 21:49:00 +0800

    update: Updated allowed hosts.

--- server/adv_ui_ocrroo_project/adv_ui_ocrroo_project/settings.py
+++ server/adv_ui_ocrroo_project/adv_ui_ocrroo_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'http://localhost'
 ]
 
 
@@ -123,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'adv_ui_ocrroo_app', 'static')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'adv_ui_ocrroo_app', 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
