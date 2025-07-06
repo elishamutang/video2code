34f3e5c9fdb3e2bf44e6abfc84c9ffb9054debf6
elishamutang <elishamutang@outlook.com>
2025-07-06 17:40:52 +0800

    update: Updated styling and implemented mobile-first design.

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -123,10 +123,10 @@
 
 	// Allow user to copy code snippet from a specific timestamp.
 	function getCode(timestampIdx: number): void {
-		const codeTarget = document.getElementById(`timestamp-${timestampIdx}`)
+		const codeTarget = document.getElementById(`timestamp-${timestampIdx}`);
 
 		if (codeTarget !== undefined && codeTarget !== null) {
-			navigator.clipboard.writeText(codeTarget.textContent ?? '')
+			navigator.clipboard.writeText(codeTarget.textContent ?? '');
 		}
 	}
 
@@ -139,12 +139,36 @@
 	});
 </script>
 
-<main class="m-5 flex gap-5">
-	<div
-		class="border border-slate-500 bg-slate-100 max-w-[1000px] flex flex-col items-center p-10 rounded-lg"
+<main
+	class="m-2 border-slate-300 rounded-lg sm:border md:p-5 max-w-7xl gap-5 sm:grid sm:grid-cols-2 sm:m-5"
+>
+	<!-- Title -->
+	<section
+		class="md:border-b md:pb-5 md:border-b-slate-300 md:my-2 my-5 flex flex-col items-center sm:col-start-1 sm:col-end-3 sm:row-start-1 sm:row-end-2"
 	>
+		<h1 class="md:text-4xl text-3xl font-semibold text-slate-500">Video2Code</h1>
+		<a class="text-sm text-slate-400" href="https://elishamutang.xyz/"
+			>by <span class="border-b border-b-slate-400 hover:text-slate-800">elishamutang</span></a
+		>
+	</section>
+
+	<!-- Brief description -->
+	<section
+		class="sm:place-self-center sm:max-w-md mb-5 border p-2 rounded-lg bg-slate-100 border-slate-500 sm:col-start-1 sm:col-end-3 sm:row-start-2 sm:row-end-3"
+	>
+		<h2 class="font-bold text-xl text-slate-600">What is this about?</h2>
+		<p class="text-slate-800">
+			A prototype to <span class="italic">detect</span> code, <span class="italic">extract</span> it
+			from a specific frame, and display it to the user in the appropriate format.
+		</p>
+	</section>
+
+	<section
+		class="sm:row-start-3 sm:row-end-4 sm:max-w-[1000px] sm:col-1 border border-slate-500 w-full bg-slate-100 flex flex-col items-center p-2 rounded-lg"
+	>
+		<!-- Form -->
 		<form
-			class="my-5 w-xl bg-slate-600 p-3 rounded-lg border flex gap-2 flex flex-col"
+			class="md:place-self-center my-5 sm:w-max-content sm:max-w-[800px] w-full bg-slate-600 p-3 rounded-lg border flex gap-2 flex flex-col"
 			id="timestampForm"
 		>
 			<h2 class="text-white text-2xl border-b pb-2 font-semibold">
@@ -164,8 +188,8 @@
 					</ul>
 				{/if}
 
-				<div class="flex gap-10 items-center justify-center w-full mb-2">
-					<div class="flex items-center gap-2 w-max mt-2">
+				<div class="flex flex-col lg:flex-row sm:gap-2 sm:justify-center items-center w-full mb-2">
+					<div class="flex items-center gap-2 w-max">
 						<label for="hours" class="text-white"> Hours: </label>
 						{#if isLoading}
 							<Skeleton class="w-[80px] min-h-[40px] bg-gray-300" />
@@ -226,15 +250,22 @@
 				type="submit"
 				disabled
 				aria-disabled="true"
-				class={`text-xl border disabled:cursor-disabled disabled:bg-gray-300 disabled:border-gray-400 border-red-500 font-semibold p-2 rounded-lg bg-red-500 text-white ${isLoading ? 'cursor-not-allowed' : 'cursor-pointer'}`}
-				onclick={getVidFrame}>Submit</button
+				class={`text-xl border disabled:cursor-disabled disabled:bg-gray-300 disabled:border-gray-400 border-red-500 font-semibold p-2 rounded-lg bg-red-500 text-white ${isLoading ? 'cursor-not-allowed opacity-75' : 'cursor-pointer'}`}
+				onclick={getVidFrame}>{isLoading ? 'Generating frame & code...' : 'Submit'}</button
 			>
 		</form>
 
-		<video controls class="my-10 max-w-[800px]">
-			<source src="http://127.0.0.1:8000/api/video/" type="video/mp4" />
-			<track kind="captions" />
-		</video>
+		<!-- Separator -->
+		<div class="border w-full border-slate-300 my-2"></div>
+
+		<!-- Video -->
+		<section class="flex flex-col items-center">
+			<h1 class="text-2xl mb-2 font-semibold text-slate-600">Video</h1>
+			<video controls class="mb-10 w-full max-w-[800px]">
+				<source src="http://127.0.0.1:8000/api/video/" type="video/mp4" />
+				<track kind="captions" />
+			</video>
+		</section>
 
 		<!-- Extracted frame here -->
 		{#if frameData.frame_url !== ''}
@@ -242,40 +273,48 @@
 				Video selected at {frameData.timestamp}
 			</h1>
 			<img
-				class="my-5 max-w-[1000px] w-full h-[500px]"
+				class="my-5 max-w-[1000px] w-full max-h-[500px]"
 				src={frameData.frame_url}
 				alt={`Extracted frame at ${frameData.timestamp}`}
 			/>
 		{/if}
-	</div>
+	</section>
 
 	<!-- Timestamps section here -->
-	<section class="border border-transparent w-full max-h-[900px] h-vh overflow-auto rounded-lg">
-		<h1 class="text-3xl p-2 font-semibold">Timestamps</h1>
-		{#each timestampCollection as _, idx}
-			{@const entry = timestampCollection[timestampCollection.length - 1 - idx]}
-			<section
-				aria-labelledby="Timestamp at 0 hours, 0 minutes, and 0 seconds."
-				class="border m-2 p-2 rounded-lg"
-			>
-				<h2 class="font-bold">
-					<span class="px-1">{timestampCollection.length - idx}</span> Timestamp at {entry.timestamp}
-				</h2>
-				<pre class="border border-slate-300 p-4 m-4 relative rounded-lg bg-slate-100">
+	<section
+		class="md:p-3 md:border-l md:border-l-slate-300 md:rounded-none sm:col-2 sm:row-start-3 sm:row-end-5 border border-transparent w-full max-h-[1000px] h-full overflow-auto rounded-lg"
+	>
+		<h1 class="md:mt-0 text-3xl m-2 mt-5 font-semibold text-slate-600">Timestamps</h1>
+		{#if timestampCollection.length === 0}
+			<p class="m-2 text-slate-500 italic">Nothing to see here...</p>
+		{:else}
+			{#each timestampCollection as _, idx}
+				{@const entry = timestampCollection[timestampCollection.length - 1 - idx]}
+				<section
+					aria-labelledby="Timestamp at 0 hours, 0 minutes, and 0 seconds."
+					class="border m-2 p-2 rounded-lg"
+				>
+					<h2 class="font-bold">
+						<span class="px-1">{timestampCollection.length - idx}.</span> Timestamp at {entry.timestamp}
+					</h2>
+					<pre class="border border-slate-300 p-4 m-4 relative rounded-lg bg-slate-100">
 				<div
-						class="p-1 text-blue-800 bg-slate-300 border border-transparent absolute top-0 left-0 rounded-tl-lg rounded-br-lg">python</div>
-				<code id={`timestamp-${timestampCollection.length - idx}`}>	
+							class="p-1 text-blue-800 bg-slate-300 border border-transparent absolute top-0 left-0 rounded-tl-lg rounded-br-lg">python</div>
+				<code
+							id={`timestamp-${timestampCollection.length - idx}`}
+							class="text-(length:--code-text) md:text-base">	
 					{entry.formatted_code}
 				</code>
 				<button
-						aria-label="Copy code from timestamp {entry.timestamp}"
-						onclick={() => getCode(timestampCollection.length - idx)}
-						type="button"
-						class="cursor-pointer top-2 right-2 absolute border py-1 px-2 bg-slate-300 rounded-md border-slate-400"
-						>Copy</button
-					>
+							aria-label="Copy code from timestamp {entry.timestamp}"
+							onclick={() => getCode(timestampCollection.length - idx)}
+							type="button"
+							class="cursor-pointer top-2 right-2 absolute border py-1 px-2 bg-slate-300 rounded-md border-slate-400"
+							>Copy</button
+						>
 				</pre>
-			</section>
-		{/each}
+				</section>
+			{/each}
+		{/if}
 	</section>
 </main>
