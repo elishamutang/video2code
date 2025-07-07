c79ad7ad8d1687ffc90645e51a8906b089687625
elishamutang <elishamutang@outlook.com>
2025-07-08 00:46:58 +0800

    update: Mocked api success and fail responses.

--- client/src/lib/helpers.ts
+++ client/src/lib/helpers.ts
@@ -1,4 +1,4 @@
-import type { Timestamp, VideoData } from './dataTypes';
+import type { FrameData, Timestamp, VideoData } from './dataTypes';
 
 function extractYoutubeVideoId(link: string): string | null {
 	const youtubeIdRegex: RegExp =
@@ -18,23 +18,82 @@ function validateTimestamp(timestamp: Timestamp, videoData: VideoData): string[]
 
 	const errors = [];
 
+	if (timestamp.hours === 0 && timestamp.minutes === 0 && timestamp.seconds === 0) {
+		errors.push(
+			'Hours, minutes and seconds cannot all be zero at the same time.'
+		)
+	}
+
 	if (timestamp.hours > videoData.duration_hours) {
 		errors.push(
-			`Invalid input: ${isPlural.hours ? 'Hours' : 'Hour'} must not exceed ${videoData.duration_hours} ${isPlural.hours ? 'hours' : 'hour'}`
+			`${isPlural.hours ? 'Hours' : 'Hour'} must not exceed ${videoData.duration_hours} ${isPlural.hours ? 'hours' : 'hour'}`
 		);
 	}
 
 	if (timestamp.minutes > videoData.duration_minutes) {
 		errors.push(
-			`Invalid input: ${isPlural.minutes ? 'Minutes' : 'Minute'} must not exceed ${videoData.duration_minutes} ${isPlural.minutes ? 'minutes' : 'minute'} `
+			`${isPlural.minutes ? 'Minutes' : 'Minute'} must not exceed ${videoData.duration_minutes} ${isPlural.minutes ? 'minutes' : 'minute'} `
 		);
 	}
 
 	if (videoData.duration_minutes >= 1 && timestamp.seconds > 60) {
-		errors.push(`Invalid input: Seconds must not exceed 60 seconds.`);
+		errors.push(`Seconds must not exceed 60 seconds.`);
 	}
 
 	return errors.length > 0 ? errors : true;
 }
 
-export { extractYoutubeVideoId, validateTimestamp };
+// Mock success API response
+function successAPIResponse(): Promise<FrameData> {
+	return new Promise((resolve, _) => {
+		setTimeout(() => {
+			const result = {
+				frame_filename: '',
+				frame_path: '',
+				frame_url: '',
+				message: '',
+				timestamp: '',
+				timestamp_seconds: 0,
+				video_filename: '',
+				extracted_code: '',
+				formatted_code: `
+class User:
+    def __init__(self, full_name, birthday):
+        self.name = full_name
+        self.birthday = birthday  # yyyymmdd
+        # Extract first and last names
+        name_pieces = full_name.split(" ")
+        self.first_name = name_pieces[0]
+        self.last_name = name_pieces[-1]
+
+user = User("Dave Bowman", 19710315)
+`
+			}
+
+			resolve(result)
+		}, 2000);
+	})
+}
+
+// Mock fail API response
+function failAPIResponse(): Promise<FrameData> {
+	return new Promise((_, reject) => {
+		setTimeout(() => {
+			const result = {
+				frame_filename: '',
+				frame_path: '',
+				frame_url: '',
+				message: '',
+				timestamp: '',
+				timestamp_seconds: 0,
+				video_filename: '',
+				extracted_code: 'OCR error: ERROR',
+				formatted_code: '',
+			}
+
+			reject(result)
+		}, 2000)
+	})
+}
+
+export { extractYoutubeVideoId, validateTimestamp, successAPIResponse, failAPIResponse };
