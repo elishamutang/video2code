16a119bf69d0ef544785ccec816dde29f9b77ba3
elishamutang <elishamutang@outlook.com>
2025-07-10 18:33:44 +0800

    update: Removed tests from helpers file.

--- client/src/lib/helpers.ts
+++ client/src/lib/helpers.ts
@@ -1,4 +1,4 @@
-import type { FrameData, Timestamp, VideoData } from './dataTypes';
+import type { Timestamp, VideoData } from './dataTypes';
 
 function extractYoutubeVideoId(link: string): string | null {
 	const youtubeIdRegex: RegExp =
@@ -43,57 +43,4 @@ function validateTimestamp(timestamp: Timestamp, videoData: VideoData): string[]
 	return errors.length > 0 ? errors : true;
 }
 
-// Mock success API response
-function successAPIResponse(): Promise<FrameData> {
-	return new Promise((resolve, _) => {
-		setTimeout(() => {
-			const result = {
-				frame_filename: '',
-				frame_path: '',
-				frame_url: '',
-				message: '',
-				timestamp: '',
-				timestamp_seconds: 0,
-				video_filename: '',
-				extracted_code: '',
-				formatted_code: `
-class User:
-    def __init__(self, full_name, birthday):
-        self.name = full_name
-        self.birthday = birthday  # yyyymmdd
-        # Extract first and last names
-        name_pieces = full_name.split(" ")
-        self.first_name = name_pieces[0]
-        self.last_name = name_pieces[-1]
-
-user = User("Dave Bowman", 19710315)
-`
-			}
-
-			resolve(result)
-		}, 2000);
-	})
-}
-
-// Mock fail API response
-function failAPIResponse(): Promise<FrameData> {
-	return new Promise((_, reject) => {
-		setTimeout(() => {
-			const result = {
-				frame_filename: '',
-				frame_path: '',
-				frame_url: '',
-				message: '',
-				timestamp: '',
-				timestamp_seconds: 0,
-				video_filename: '',
-				extracted_code: 'OCR error: ERROR',
-				formatted_code: '',
-			}
-
-			reject(result)
-		}, 2000)
-	})
-}
-
-export { extractYoutubeVideoId, validateTimestamp, successAPIResponse, failAPIResponse };
+export { extractYoutubeVideoId, validateTimestamp };
