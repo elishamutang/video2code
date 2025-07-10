e7453208b75edd1a258c5498def1d475e1ed86ed
elishamutang <elishamutang@outlook.com>
2025-07-11 00:35:40 +0800

    update: Updated tests.

--- client/src/lib/tests.ts
+++ client/src/lib/tests.ts
@@ -1,19 +1,16 @@
 import type { FrameData } from "./dataTypes";
 
-// Mock success API response
-function successAPIResponse(): Promise<FrameData> {
-    return new Promise((resolve, _) => {
-        setTimeout(() => {
-            const result = {
-                frame_filename: '',
-                frame_path: '',
-                frame_url: '',
-                message: '',
-                timestamp: '00:05:30',
-                timestamp_seconds: 330,
-                video_filename: '',
-                extracted_code: '',
-                formatted_code: `
+const originalData = [
+    {
+        frame_filename: '',
+        frame_path: '',
+        frame_url: '',
+        message: '',
+        timestamp: '00:05:30',
+        timestamp_seconds: 330,
+        video_filename: '',
+        extracted_code: '',
+        formatted_code: `
 class User:
     def __init__(self, full_name, birthday):
         self.name = full_name
@@ -25,10 +22,60 @@ class User:
 
 user = User("Dave Bowman", 19710315)
 `
-            }
+    },
+    {
+        frame_filename: '',
+        frame_path: '',
+        frame_url: '',
+        message: '',
+        timestamp: '00:05:30',
+        timestamp_seconds: 330,
+        video_filename: '',
+        extracted_code: '',
+        formatted_code: `
+class Teacher:
+    def __init__(self, full_name, birthday):
+        self.name = full_name
+        self.birthday = birthday  # yyyymmdd
 
-            resolve(result)
-        }, 2000);
+teacher = Teacher("Dave Bowman", 19710315)
+`
+    },
+    {
+        frame_filename: '',
+        frame_path: '',
+        frame_url: '',
+        message: '',
+        timestamp: '00:05:30',
+        timestamp_seconds: 330,
+        video_filename: '',
+        extracted_code: '',
+        formatted_code: `
+class Student:
+    def __init__(self, full_name, birthday):
+        self.name = full_name
+        self.birthday = birthday  # yyyymmdd
+`
+    }
+]
+
+let dataCopy = [...originalData]
+
+// Mock success API response
+function successAPIResponse(): Promise<FrameData> {
+    let response: FrameData
+
+    if (dataCopy.length === 0) {
+        dataCopy = [...originalData]
+    }
+
+    response = dataCopy.shift() as FrameData
+    console.log(dataCopy)
+
+    return new Promise((resolve, _) => {
+        setTimeout(() => {
+            resolve(response)
+        }, 2000)
     })
 }
 
