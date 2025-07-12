091695f9afa06078e33a9cc118ef40ed578891e3
elishamutang <elishamutang@outlook.com>
2025-07-12 11:33:07 +0800

    update: Removed throttle test URL.

--- server/adv_ui_ocrroo_project/adv_ui_ocrroo_app/urls.py
+++ server/adv_ui_ocrroo_project/adv_ui_ocrroo_app/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('media/video/', views.serve_video, name='serve_video'),
     path('media/video/frame/<str:timestamp>/', views.get_vid_frame, name='get_vid_frame'),
-    path('video/duration/', views.get_vid_duration, name='get_vid_duration'),
-    path('throttle/', views.throttle_test, name='throttle_test')
+    path('video/duration/', views.get_vid_duration, name='get_vid_duration')
 ]
